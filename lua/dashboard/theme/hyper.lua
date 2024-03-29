2c4f6cb7f5068d899ac2b577d6de577cd8b00d4d
ilan schemoul <ilanschemoul@gmail.com>
2024-03-29 05:21:26 +0100

    feat: add editor command DashboardUpdateFooter

--- lua/dashboard/theme/hyper.lua
+++ lua/dashboard/theme/hyper.lua
@@ -455,11 +455,11 @@ local function gen_footer(config)
 
   local first_line = api.nvim_buf_line_count(config.bufnr)
   api.nvim_buf_set_lines(config.bufnr, first_line, -1, false, utils.center_align(footer))
-
-  ---@diagnostic disable-next-line: param-type-mismatch
   for i, _ in pairs(footer) do
     api.nvim_buf_add_highlight(config.bufnr, 0, 'DashboardFooter', first_line + i - 1, 0, -1)
   end
+
+  utils.add_update_footer_command(config.bufnr, footer)
 end
 
 local function project_delete()
@@ -511,7 +511,7 @@ local function theme_instance(config)
       - 2
     local fill = utils.generate_empty_table(size)
     api.nvim_buf_set_lines(config.bufnr, 0, 0, false, fill)
-    vim.bo[config.bufnr].modifiable = false
+    vim.bo[config.bufnr].modifiable = true
     vim.bo[config.bufnr].modified = false
     --defer until next event loop
     vim.schedule(function()
